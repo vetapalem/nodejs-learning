c98c7fc330080371f83cde57869cd9e2763213a3
vetapalem <krishnavetapalem2019@gmail.com>
2023-11-02 22:27:08 +0530

    noder http headers

--- node_project/first.js
+++ node_project/first.js
@@ -5,7 +5,7 @@ var dat= require('./first_modules'); // importing modules in this way...
 
 http.createServer(
     function(reques,response){
-        response.writeHead(200,{'content-Type':'text/html'});
+        response.writeHead(200,{'content-Type':'text/html'});// this is http header...
         response.end(`${"<h1>hello world</h1>"} ${dat.datetime()}`);
         // response.write(reques.url)
         // response.end(`<h1>${reques.url}</h1>`)
