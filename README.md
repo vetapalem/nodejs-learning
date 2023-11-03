1d2abe4409732481985e07d80243a4a14a0f0f3c
vetapalem <krishnavetapalem2019@gmail.com>
2023-11-03 18:23:17 +0530

    server console log

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # **Node learning**
 
-learning stage -1
+#### learning stage -1
 
 simple create a server by help of  nodejs
 
@@ -8,7 +8,7 @@ simple create a server by help of  nodejs
 
 
 
-learning stage -2
+#### learning stage -2
 
 * http headers
 
@@ -20,7 +20,7 @@ response.writeHead(200,{'content-Type':'text/html'});// this is http header...
 
 
 
-learning stage -3
+#### learning stage -3
 
 how export module's in node js ?
 
@@ -41,5 +41,20 @@ exports.`<object>`
 import object
 
 ```
-var dat= require('./first_modules');// importing of object
+ var dat= require('./first_modules');// importing of object 
+```
+
+---
+
+#### learning stage -4
+
+how to create a live console log  in server..
+
+eg :-
+
+ip methods { GET | PUT | PATCH | DELETE ....}  date in GMT format url parameters like {/hi | /bye ....}
+
+```
+console.log(`${'http://127.0.0.1:3000'}\t${reques.method}\t${Date()}\t ${reques.url}`)
+
 ```
--- node_project/first.js
+++ node_project/first.js
@@ -6,10 +6,14 @@ var dat= require('./first_modules'); // importing modules in this way...
 http.createServer(
     function(reques,response){
         response.writeHead(200,{'content-Type':'text/html'});// this is http header...
-        response.end(`${"<h1>hello world</h1>"} ${dat.datetime()}`);
+        response.end(`${"<h1>hello world</h1>"}\n <h2>${dat.datetime()}</h2>\n <h1><q>${reques.url}</q></h1>`);
         // response.write(reques.url)
         // response.end(`<h1>${reques.url}</h1>`)
-        console.log(reques.url)
+        // console.log(reques.url)
+        // console.log()
+        // creating log information ..
+        console.log(`${'http://127.0.0.1:3000'}\t${reques.method}\t${Date()}\t ${reques.url}`)
+        // console.log(reques.headers,reques.method)
     }
 ).listen(3000)
-console.log(`${'http://127.0.0.1:3000'} \n ${"reques.url"}`)
+console.log(`${'http://127.0.0.1:3000'} \t ${"reques.url"}`)
